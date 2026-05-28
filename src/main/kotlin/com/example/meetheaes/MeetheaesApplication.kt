ba296101daa2bbbc8306dc0631a98147f128615c
Kellyane L. Diakumpuna <yauth11@gmail.com>
2026-05-28 22:32:19 +0200

    configuration du projet w/ springinitializr

---
+++ src/main/kotlin/com/example/meetheaes/MeetheaesApplication.kt
@@ -0,0 +1,11 @@
+package com.example.meetheaes
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MeetheaesApplication
+
+fun main(args: Array<String>) {
+	runApplication<MeetheaesApplication>(*args)
+}
